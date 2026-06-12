c8ff739d98d23e0bc377d7a6e8bfe8a7a4e3fd73
Your Name <you@example.com>
2026-06-12 09:33:14 +0000

    Allow all origins

--- index.ts
+++ index.ts
@@ -19,12 +19,15 @@ async function startServer() {
   const listingCount = await repo.countListings();
   console.log(`Database ready with ${userCount} users and ${listingCount} listings.`);
 
+ //allow all origins
   app.use(
-    cors({
-      origin: process.env.CORS_ORIGIN || "http://localhost:5173",
-      credentials: true,
-    })
-  );
+  cors({
+    origin: true, // reflects the requesting origin
+    credentials: true,
+  })
+);
+
+  
   app.use(express.json({ limit: "50mb" }));
   app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
